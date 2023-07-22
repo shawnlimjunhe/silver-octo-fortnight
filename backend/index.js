289e589c7bee942b0c673ed980a4e6aff8be556c
shawnlimjunhe <46809501+shawnlimjunhe@users.noreply.github.com>
2023-07-22 23:10:10 +0800

    Add test call to coinbase API

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const axios = require('axios');
 
 
 const app = express();
 const port = 8000;
+const targetUrl = "https://api.coinbase.com/v2/exchange-rates";
+
+const crypto = ['BTC', 'DOGE', 'ETH'];
+const fiat = ['USD', 'SGD', 'EUR'];
+
+app.get('/exchange-rates', async (req, res) => {
+  const { base } = req.query;
+
+  const response = await axios.get(targetUrl);
+  
+  console.log(response.data);
+
+  res.json({'queryParam': base});
+})
 
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
