192ccc2c9c49e4cf87528dd7c8f0c60ff7d0aa31
shawnlimjunhe <46809501+shawnlimjunhe@users.noreply.github.com>
2023-07-24 15:21:33 +0800

    Fixed issue with event propagation

--- frontend/src/itemBox.js
+++ frontend/src/itemBox.js
@@ -10,6 +10,7 @@ const ItemBox = ({item}) => {
   const [isBoxExpanded, setIsBoxExpanded] = useState(false);
   const [logoUrl, setLogoUrl] = useState('');
   const [providerTitle, setProviderTitle] = useState('')
+  const [providerData, setProviderData] = useState({});
 
   const expandedColour = '#1A2632';
   const collapseColour = '#42607B';
@@ -18,19 +19,22 @@ const ItemBox = ({item}) => {
     background: isBoxExpanded ? expandedColour : collapseColour
   } 
 
-  const handleChevronClick = async (provider_name) => {
+  const handleChevronClick = async (provider_name, event) => {
+    event.stopPropagation();
     try {
-      const response = await axios.get(`https://api.apis.guru/v2/${provider_name}.json`)
-      const data = response.data.apis
-      const key = Object.keys(data).find((key) => key.includes(provider_name))
-      const providerData = data[key]
 
-      const providerInfo = providerData.info;
+      if (providerTitle === '') {
+        console.log(`Retrieving data of ${provider_name}`)
+        const response = await axios.get(`https://api.apis.guru/v2/${provider_name}.json`)
+        const data = response.data.apis
+        const key = Object.keys(data).find((key) => key.includes(provider_name))
+        const extractedProviderData = data[key]
+        setProviderData(extractedProviderData)
+        const providerInfo = extractedProviderData.info;  
+        setLogoUrl(providerInfo['x-logo'].url);
+        setProviderTitle(providerInfo.title)
+      }
 
-      setLogoUrl(providerInfo['x-logo'].url);
-      setProviderTitle(providerInfo.title)
-
-      console.log(providerData);
       setIsBoxExpanded(!isBoxExpanded)
       console.log('Chevron Clicked', provider_name);
     } catch (error) {
@@ -44,9 +48,9 @@ const ItemBox = ({item}) => {
       <div className="item-container" style={backgroundStyle}>
         <div className="item-row">
           {item}
-          <FontAwesomeIcon icon={isBoxExpanded ? faChevronUp : faChevronDown} size="2xs" onClick={() => handleChevronClick(item)} />
+          <FontAwesomeIcon className="drop-down" icon={isBoxExpanded ? faChevronUp : faChevronDown} size="2xs" onClick={(event) => handleChevronClick(item, event)} />
         </div>
-        {isBoxExpanded && <ProviderOverview title={providerTitle} logoUrl={logoUrl}/>}
+        {isBoxExpanded && <ProviderOverview title={providerTitle} logoUrl={logoUrl} providerData={providerData}/>}
       </div>
     </li>
   )
--- frontend/src/providerOverview.js
+++ frontend/src/providerOverview.js
@@ -1,9 +1,13 @@
 import React from "react"
 import './providerOverview.css'
 
-const ProviderOverview = ({title, logoUrl}) => {
+const ProviderOverview = ({title, logoUrl, providerData}) => {
+  const handleOnClick = (providerData) => {
+    console.log(providerData)
+  }
+  
   return (
-    <div className="parent-container">
+    <div className="parent-container" onClick={() => handleOnClick(providerData)}>
       <img src={logoUrl} alt='' className="logo"></img>
       <span className="title">{title}</span>
     </div>
